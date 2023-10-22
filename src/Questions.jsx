36100880a4b367f651c02211bbf83f943c45ea64
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2023-10-22 13:03:35 +0530

    fifth commit

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -6,19 +6,19 @@ const Questions = ({ questions, start }) => {
 
     /*  Answers state for storing all the shuffled answer*/
     const [answers, setAnswers] = React.useState([]);
-
+    const [selectedAnswers, setSelectedAnswers] = React.useState({ qIndex: null, useranswer: null })
+    const [correctCount, setCorrectCount] = React.useState(0)
+    const [toggle, setToggle] = React.useState(false)
 
     React.useEffect(() => {
         const shuffledAnswers = questions.map(q =>
             shuffleArray([...q.incorrect_answers, q.correct_answer])
         );
 
-        console.log(shuffledAnswers)
 
         /*Updating answer state with shuffled answer */
 
         setAnswers(shuffledAnswers);
-        console.log(answers)
     }, [questions]);
 
     /*ShuffleArray function for Shuffling correct and incorrect answers */
@@ -51,6 +51,21 @@ const Questions = ({ questions, start }) => {
         const selectedElementId = e.target.id
         const selectedElement = document.getElementById(selectedElementId)
         updateSelectStyle(selectedElement)
+
+        setSelectedAnswers((prev) => {
+            if (prev !== null) {
+                return {
+                    ...prev,
+                    [questionIndex]: selectedElementId, // Assuming you want to update the questionIndex with selectedElementId
+                };
+            } else {
+                return {
+                    [questionIndex]: selectedElementId,
+                };
+            }
+        });
+
+
     }
 
     let quizQuestions;
@@ -78,18 +93,30 @@ const Questions = ({ questions, start }) => {
         })
     }
 
-    /*function checkAnswers() {
-
+    function checkAnswers() {
+        console.log(selectedAnswers)
+        setToggle(true)
         questions.forEach((question, index) => {
+            const userAnswer = selectedAnswers[index]
+            document.getElementById(question.correct_answer).style.backgroundColor = 'green'
+
+            if (userAnswer !== undefined) {
+                if (question.correct_answer === userAnswer) {
+                    setCorrectCount(correctCount + 1);
+                } else if (question.correct_answer !== userAnswer) {
+                    document.getElementById(userAnswer).style.backgroundColor = 'red'
+                }
+            }
 
 
 
         })
-    }*/
+    }
     return (
         <div className='quiz-div'>
             {start && quizQuestions}
-            {start && < button className='check-btn' /*onClick={checkAnswers}*/>Check Answer</button>}
+            {start && < button className='check-btn' onClick={checkAnswers}>Check Answer</button>}
+            {toggle && <h3>you got {correctCount}/5 correct Answers</h3>}
         </div>
     )
 }
